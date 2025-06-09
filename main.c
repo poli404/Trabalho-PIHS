16e461125e68eb68cfac44d369dd0992af041ee3
Poli404 <ra134539@uem.br>
2025-06-09 14:21:25 -0300

    Documentação

--- main.c
+++ main.c
@@ -1,24 +1,34 @@
+/**
+ * Aluna: Maria Eduarda de Mello Policante - RA134539
+ * Disciplina: Programação para Interfaceamento de Hardware e Software
+*/
+
 #include <stdio.h>
 #include "pocketpy.h"
 #include "levenshtein.h"
 
 bool py_levenshtein(int argc, py_Ref argv) {
+    /**
+     * Função de extensão da função de levenshtein para a chamada em Python.
+    */
 
-    PY_CHECK_ARGC(2);
-    PY_CHECK_ARG_TYPE(0, tp_str);
+    // checagem dos argumentos passados pelo script Python para a função levenshtein
+    PY_CHECK_ARGC(2); // número
+    PY_CHECK_ARG_TYPE(0, tp_str); // tipo
     PY_CHECK_ARG_TYPE(1, tp_str);
 
-    const char* _s1 = py_tostr(py_arg(0));
+    const char* _s1 = py_tostr(py_arg(0)); // obtenção dos parâmetros para a função de levenshtein
     const char* _s2 = py_tostr(py_arg(1));
 
-    int res = levenshtein(_s1, _s2);
+    int res = levenshtein(_s1, _s2); // aplicação da função da biblioteca levenshtein.h
 
-    py_newint(py_retval(), res);
+    py_newint(py_retval(), res); // extenção do resultado obtido pela função para o script Python
 
     return true;  
 }
 
 int main(){
+    // Leitura do script Python
     FILE *arq = fopen("script.py", "r");
     char program[10000];
 
@@ -26,12 +36,12 @@ int main(){
 
     fclose(arq);
 
-    py_initialize();
+    py_initialize(); // Inicialização do interpretador embutido Python
 
     py_GlobalRef mod = py_getmodule("__main__");
-    py_bindfunc(mod, "levenshtein", py_levenshtein);
+    py_bindfunc(mod, "levenshtein", py_levenshtein); // definição da assinatura da função no script Python
 
-    if (!py_exec(program, "", EXEC_MODE, NULL)) {
+    if (!py_exec(program, "", EXEC_MODE, NULL)) { // execução do programa Python
         py_printexc();
     }
 
--- script.py
+++ script.py
@@ -3,7 +3,8 @@ str12 = "bolo"
 str21 = "careta"
 str22 = "vareta"
 
-print(f"Distância de Levenshtein entre '{str21}' e '{str22}':", levenshtein(str21, str22))
-print(f"Distância de Levenshtein entre '{str11}' e '{str12}':", levenshtein(str11, str12))
-print(f"Distância de Levenshtein entre '{str11}' e '{str21}':", levenshtein(str11, str21))
-print(f"Distância de Levenshtein entre '{str22}' e '{str12}':", levenshtein(str22, str12))
+print(f"Distância de Levenshtein entre '{str11}' e '{str11}':", levenshtein(str11, str11)) # 0 - mesma palavra
+print(f"Distância de Levenshtein entre '{str21}' e '{str22}':", levenshtein(str21, str22)) # 1
+print(f"Distância de Levenshtein entre '{str11}' e '{str12}':", levenshtein(str11, str12)) # 2
+print(f"Distância de Levenshtein entre '{str11}' e '{str21}':", levenshtein(str11, str21)) # 4
+print(f"Distância de Levenshtein entre '{str12}' e '{str22}':", levenshtein(str12, str22)) # 6 - todas as letras mudam
